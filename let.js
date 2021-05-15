50a68b33fe9b93fcfba79c51de656827930b2465
blackzwashere <sapahub@gmail.com>
2021-05-15 20:46:28 +0800

    vperbandingan var ES5 dan let ES6

---
+++ let.js
@@ -0,0 +1,11 @@
+// ES5
+var myName = "Candra Pratama";
+// ES5 var melakukan replace
+// myName = "Belajar Koding"
+// console.log(myName);
+
+// ES6
+let myCar = "RWB";
+// ES6 let akan melakukan replace atau perubahan sama seperti var di ES5
+myCar = "Kijang";
+console.log(myCar);
