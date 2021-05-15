3ab309763917890f1bb5faf2165a3ba47dfcdf8d
blackzwashere <sapahub@gmail.com>
2021-05-15 20:34:51 +0800

    perbedaan var dan const

---
+++ var-const.js
@@ -0,0 +1,11 @@
+// ES5
+var myName = "Candra Pratama";
+// ES5 melakukan replace
+myName = "Belajar Koding"
+console.log(myName);
+
+// ES6
+const myCar = "RWB";
+// ES6 tidak akan melakukan replace
+myCar = "Kijang";
+console.log(myCar);
